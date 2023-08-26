778d4639932163d3a2f21b9ecc05dbebd5a7b7d8
halvetal <75677306+halvetal@users.noreply.github.com>
2023-08-26 18:00:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,74 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import json
+import openai
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pres.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-app.app_context().push()
-
-
-class Presentation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(30))
-    problem_user = db.Column(db.String(500))
-    problem_gpt = db.Column(db.String(500))
-    description = db.Column(db.Text)
-    adv = db.Column(db.Text)
-    value_proposition = db.Column(db.Text)
-    desc_vp_gpt = db.Column(db.Text)
-    product = db.Column(db.Text)
-    industry = db.Column(db.String(300))
-    tam = db.Column(db.Integer)
-    sam = db.Column(db.Integer)
-    som = db.Column(db.Integer)
-    bm = db.Column(db.Text)
-    APRU = db.Column(db.Integer)
-    lt = db.Column(db.Integer)
-    ltv = db.Column(db.Integer)
-    finance = db.Column(db.Text)
-    team = db.Column(db.Text)
-    investors = db.Column(db.Text)
-    investmens = db.Column(db.Text)
-    roadmap = db.Column(db.Text)
-    contact = db.Column(db.Text)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
+openai.api_key = ""
 
 @app.route('/w',methods = ['POST', 'GET'])
 def w():
+
     if request.method == 'POST':
         return ''
     else:
-        try:
             project_name = request.json['project_name']
             problem = request.json['problem']
-            za = request.json['za']
-            description = request.json['description']
-            adv = request.json['adv']
-            product = request.json['product']
-            team = request.json['team']
-            investors = request.json['investors']
-            value_of_investments = request.json['value_of_investments']
-            where_inv = request.json['where_inv']
-            roadmap = request.json['roadmap']
-            phone = request.json['phone']
-            email = request.json['email']
-            website = request.json['website']
+
+
             #industry for tam sam som нет на сайте
-            APRU = 1  # выручка на число клиентов
+            APRU = 1  # выручка / число клиентов
             churn_rate = 1
             lt = 1 / churn_rate
             ltv = APRU * lt
-            presentation = Presentation(project_name=project_name)
-            try:
-                db.session.add(presentation)
-                db.session.commit()
-                return '2'
-            except:
-                return '4'
-        except:
-            return ''
+            with open(f'json.json', 'w') as f:
+                rep = {'project_name': project_name, 'problem': problem}
+                js = json.dumps(rep, ensure_ascii=False)
+                f.write(js)
+            return '1'
+
+
+@app.route('/project_name',methods = ['POST', 'GET'])
+def project_name():
+    if request.method == 'GET':
+        with open('json.json', 'r', encoding='utf-8') as f:
+            text = json.load(f)
+            return text['project_name']
+
+
+@app.route('/problem',methods = ['POST', 'GET'])
+def problem():
+    if request.method == 'GET':
+        with open('json.json', 'r', encoding='utf-8') as f:
+            text = json.load(f)
+            model_engine = "text-davinci-003"
+            prompt = "Сфорулируй проблематику более грамотно: "+text['problem']
+
+            max_tokens = 1024
+
+            # генерируем ответ
+            completion = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt,
+                max_tokens=max_tokens,
+                temperature=0.5,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+
+            # выводим ответ
+            return completion.choices[0].tex
+
+@app.route('/za',methods = ['POST', 'GET'])
+def problem():
+    if request.method == 'GET':
+        with open('json.json', 'r', encoding='utf-8') as f:
+            text = json.load(f)
+            return text['za']
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
