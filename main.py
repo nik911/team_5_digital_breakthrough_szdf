b8b7a092b889f7ca3e824d0c89c9d1be9145c0ea
Nikolai Babuhin <nikolai.babuxin@gmail.com>
2023-08-26 20:29:22 +0300

    [FEAT] sh

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import subprocess
 
 command = '/bin/sh ./script_presa.sh'
 os.system(command)
+
+
+
+def final_presa():
+    command = '/bin/sh ./script_presa.sh'
+    os.system(command)
+    # выводим ответ
+    return completion.choices[0].tex
--- script_presa.sh
+++ script_presa.sh
@@ -4,16 +4,20 @@ marp --pdf slide-deck.md
 marp pattern1.md -o pattern1.pdf
 marp --pptx slide-deck.md
 marp pattern1.md -o pattern1.pptx
+marp pattern1.md -o pattern1.png --image-scale 1
+
+
 
 marp --pdf slide-deck.md
 marp pattern2.md -o pattern2.pdf
 marp --pptx slide-deck.md
 marp pattern2.md -o pattern2.pptx
-
+marp pattern2.md -o pattern2.png --image-scale 1
 
 marp --pdf slide-deck.md
 marp pattern3.md -o pattern3.pdf
 marp --pptx slide-deck.md
 marp pattern3.md -o pattern3.pptx
+marp pattern3.md -o pattern3.png --image-scale 1
 
 echo -n "Done!"
