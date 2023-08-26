ec6375f2904415109a02858c9927cf3095acad20
Nikolai Babuhin <nikolai.babuxin@gmail.com>
2023-08-26 10:25:16 +0300

    [FEAT] logo

---
+++ cv/log_deepai.py
@@ -0,0 +1,13 @@
+
+import requests
+
+
+
+r = requests.post(
+    "https://api.deepai.org/api/logo-generator",
+    data={
+        'text': 'IT company',
+    },
+    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+)
+print(r.json())
