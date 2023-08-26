a218cb7f939c9280cc4895971adc510a819a9dee
Nikolai Babuhin <nikolai.babuxin@gmail.com>
2023-08-26 21:02:51 +0300

    [FIX] front sh

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import subprocess
 command = '/bin/sh ./script_presa.sh'
 os.system(command)
 
-
-
 def final_presa():
     command = '/bin/sh ./script_presa.sh'
     os.system(command)
--- script_presa.sh
+++ script_presa.sh
@@ -2,7 +2,7 @@
 
 marp --pdf front/src/pattern1.md
 marp mdFiles/pattern1.md -o front/src/pattern1.pdf
-marp --pptx mdFiles/pattern1.md
+marp --pptx front/src/pattern1.md
 marp mdFiles/pattern1.md -o front/src/pattern1.pptx
 marp mdFiles/pattern1.md -o front/src/pattern1.md --image-scale 1
 
@@ -12,10 +12,10 @@ marp --pptx front/src/pattern2.md
 marp mdFiles/pattern2.md -o front/src/pattern2.pptx
 marp mdFiles/pattern2.md -o front/src/pattern2.png --image-scale 1
 
-marp --pdf mdFiles/pattern3.md
+marp --pdf front/src/mdFiles/pattern3.md
 marp mdFiles/pattern3.md -o front/src/pattern3.pdf
-marp --pptx pattern3.md
+marp --pptx front/src/pattern3.md
 marp mdFiles/pattern3.md -o front/src/pattern3.pptx
 marp mdFiles/pattern3.md -o front/src/pattern3.png --image-scale 1
 
-echo -n "Done!"
+echo "Done!"
